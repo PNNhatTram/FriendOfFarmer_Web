07bf707b62d3a6be3cc150ff8097edbc1d6e15df
Phạm Minh Tân <115931062+TanPMT@users.noreply.github.com>
2024-04-26 15:33:47 +0700

    Update settings.py

--- FOF/FOF/settings.py
+++ FOF/FOF/settings.py
@@ -172,5 +172,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bemankda@gmail.com'
-EMAIL_HOST_PASSWORD = 'jxyqxhojugiifbvw'
+EMAIL_HOST_USER = 'fof.nt208@gmail.com'
+EMAIL_HOST_PASSWORD = 'trvdreyqfacobedm'
