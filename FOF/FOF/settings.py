03bb51ca4fc15c5e172778cff7c3ae366c79fb56
Phạm Minh Tân <22521310@gm.uit.edu.vn>
2024-04-27 22:18:21 +0700

    fix mail

--- FOF/FOF/settings.py
+++ FOF/FOF/settings.py
@@ -173,4 +173,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'fof.nt208@gmail.com'
-EMAIL_HOST_PASSWORD = 'trvdreyqfacobedm'
+EMAIL_HOST_PASSWORD = 'ljzjokxdoluzzwyg'
