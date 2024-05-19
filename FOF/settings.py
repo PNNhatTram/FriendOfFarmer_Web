c49bb3d8d9bdb183f14a3430c45ca49cf7f9740a
Phạm Minh Tân <115931062+TanPMT@users.noreply.github.com>
2024-05-19 22:14:33 +0700

    Update settings.py

--- FOF/settings.py
+++ FOF/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q((2jp@*d5tdk-8s4-!(x)$c6+j7%z^%1x11_dn&lt@kb8r&s3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["foft4k.me", "www.foft4k.me"]
+ALLOWED_HOSTS = ["127.0.0.1","foft4k.me", "www.foft4k.me"]
 
    # settings.py
 
